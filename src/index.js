5f512ce837e730075f484c41dc5128f331b35be3
John Paul <lucas.gabule@gmail.com>
2023-03-12 20:53:31 +0800

    taking masterclass on stripe but it didn't discuss the subscription part

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductsContextProvider from './context/products-context';
+import CartContextProvider from './context/cart-context';
+import UserContextProvider from './context/user-context';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ProductsContextProvider>
+      <CartContextProvider>
+        <Elements stripe={stripePromise}>
+          <UserContextProvider>
+            <App />
+          </UserContextProvider>
+        </Elements>
+      </CartContextProvider>
+    </ProductsContextProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
